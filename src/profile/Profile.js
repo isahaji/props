7f1797b36df875e8aff18a207f5061db251078f6
isa haji <Isa@isahaji.com>
2021-08-08 18:34:38 +0300

    added button function

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -6,14 +6,20 @@ import React from 'react'
 
 
 const Profile = (props) => {
+    
     return (
         <div>
             <img src={props.children} alt="this is the name" />
             <h1>{props.fullName}</h1>
             <h1>{props.bio} </h1>
             <h1>{props.profession} </h1>
+            <button onClick={alet} > Click me {props.fullName} </button>
         </div>
     )
+}
+
+const alet = props => {
+    alert(`hi there ${props.fullName}`)
 }
 
 export default Profile
